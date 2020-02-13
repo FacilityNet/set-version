6f2e1b8d6c6ee8e70250076ef8bf22bb0057a486
Søren Boisen <soeren.boisen@cateringportal.dk>
2020-02-13 13:32:53 +0100

    Cleanup
     - cleanup: dist should not be in repository on master branch
     - cleanup: attempt to normalize line endings
     - fix: do not take any global .eslintrc file into account

--- src/git.ts
+++ src/git.ts
@@ -1,41 +1,41 @@
 import * as exec from '@actions/exec'
 
 interface ProcessOutput {
-    stdout: string,
-    stderr: string,
+  stdout: string
+  stderr: string
 }
 
 async function invoke(cmd: string, params: string[]): Promise<ProcessOutput> {
-    let output: ProcessOutput = { stdout: '', stderr: '' }
-    const opts = {
-        listeners: {
-            stdout: (data: Buffer) => output.stdout += data.toString(),
-            stderr: (data: Buffer) => output.stderr += data.toString(),
-        }
+  const output: ProcessOutput = {stdout: '', stderr: ''}
+  const opts = {
+    listeners: {
+      stdout: (data: Buffer) => (output.stdout += data.toString()),
+      stderr: (data: Buffer) => (output.stderr += data.toString())
     }
-  
-    await exec.exec(cmd, params, opts)
-    return output
+  }
+
+  await exec.exec(cmd, params, opts)
+  return output
 }
 
 export interface Git {
-    describe(glob: string): Promise<string>
+  describe(glob: string): Promise<string>
 }
 
 export class GitError extends Error {
-    constructor(message?: string) {
-        super(message)
-        // Restore prototype chain
-        Object.setPrototypeOf(this, new.target.prototype)
-    }
+  constructor(message?: string) {
+    super(message)
+    // Restore prototype chain
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
 }
 
 const impl: Git = {
-    async describe(glob) {
-        const { stdout, stderr } = await invoke('git', ['describe', '--match', glob])
-        if (stderr.startsWith('fatal')) throw new GitError(stderr)
-        return stdout
-    }
+  async describe(glob) {
+    const {stdout, stderr} = await invoke('git', ['describe', '--match', glob])
+    if (stderr.startsWith('fatal')) throw new GitError(stderr)
+    return stdout
+  }
 }
 
 export default impl
--- src/version.ts
+++ src/version.ts
@@ -1,33 +1,36 @@
-import { Git } from './git'
+import {Git} from './git'
 
 const dashRegex = /-/
 const dotRegex = /\./
 
 interface SemanticVersion {
-    major: string,
-    minor: string,
-    patch: string,
-    preRelease?: string,
-    buildMetadata?: string,
+  major: string
+  minor: string
+  patch: string
+  preRelease?: string
+  buildMetadata?: string
 }
 
 export async function getVersionFromGit(git: Git): Promise<SemanticVersion> {
-    const described = await git.describe("v*")
-    const [ majorMinor, commitsSinceTag, sha1 ] = described.split(dashRegex)
-    const [ major, minor ] = majorMinor.substring(1).split(dotRegex)
+  const described = await git.describe('v*')
+  const [majorMinor, commitsSinceTag, sha1] = described.split(dashRegex)
+  const [major, minor] = majorMinor.substring(1).split(dotRegex)
 
-    return {
-        major,
-        minor,
-        patch: commitsSinceTag ?? "",
-        preRelease: "",
-        buildMetadata: sha1 ?? "",
-    }
+  return {
+    major,
+    minor,
+    patch: commitsSinceTag ?? '',
+    preRelease: '',
+    buildMetadata: sha1 ?? ''
+  }
 }
 
-export function stringify(v: SemanticVersion, includeMetadata: boolean): string {
-    let res = `${v.major}.${v.minor}.${v.patch}`
-    if (v.preRelease != null) res += `-${v.preRelease}`
-    if (includeMetadata && v.buildMetadata != null) res += `+${v.buildMetadata}`
-    return res
+export function stringify(
+  v: SemanticVersion,
+  includeMetadata: boolean
+): string {
+  let res = `${v.major}.${v.minor}.${v.patch}`
+  if (v.preRelease != null) res += `-${v.preRelease}`
+  if (includeMetadata && v.buildMetadata != null) res += `+${v.buildMetadata}`
+  return res
 }
